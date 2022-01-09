a0d21f068dda28ab23f2cf6fdff1eb0f24de5e4f
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-01-09 14:57:38 +0100

    Add files via upload

---
+++ Months/script.js
@@ -0,0 +1,9 @@
+console.log('Number To Month');
+
+function numberToMonth() {
+    let months =  ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let number = months[10];
+
+    return number;
+}
+console.log(numberToMonth());
